9e768620cdaa1af801ace3a0d05c05068b356b01
Léa T <94976273+Lea2210@users.noreply.github.com>
2023-01-08 00:41:17 +0100

    Create Project.js

---
+++ models/Project.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("./db");
+
+class Project extends Model {}
+
+Project.init(
+  {
+    //name of the project
+    nameProject: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+    //Name of the person in charge of the project
+    name: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+    //project description
+    description: DataTypes.TEXT,
+
+    //Project status (in progress, completed)
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: "à faire",
+    },
+
+     //project start date
+     startDate: DataTypes.DATE,
+
+    //project end date
+    dueDate: DataTypes.DATE,
+
+    //Project ID
+    Id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+  },
+  {
+    sequelize: connection,
+  }
+);
+
+module.exports = Project;
