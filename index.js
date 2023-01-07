3b6db9b5947c37ce5053c90a8fdc2a2b2b8e5116
Léa T <94976273+Lea2210@users.noreply.github.com>
2023-01-08 00:48:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const checkRequestFormat = require(".Server/middlewares/checkRequestFormat");
 const userRouter = require(".Server/Routes/users");
 const employeeRouter = require(".Server/Routes/employee");
 const tachesRouter = require(".Server/Routes/tâches");
-const securityRouter = require(".Server/Routes/security");
+const projectRouter = require(".Server/Routes/project");
 require("./models/db");
 
 const app = express();
@@ -11,10 +11,10 @@ const app = express();
 app.use(checkRequestFormat);
 app.use(express.json());
 
-app.use(securityRouter);
 app.use(userRouter);
 app.use(employeeRouter);
 app.use(tachesRouter);
+app.use(projectRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
