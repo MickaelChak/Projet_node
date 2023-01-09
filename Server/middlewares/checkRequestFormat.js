1258c04320c72fe3602fbaeafa8144ace7d3ec28
mchaksou <103125215+MickaelChak@users.noreply.github.com>
2023-01-09 15:22:25 +0100

    Delete Server/middlewares directory

--- Server/middlewares/checkRequestFormat.js
+++
@@ -1,8 +0,0 @@
-module.exports = (request, response, next) => {
-  if (request.method === "POST" || request.method === "PUT") {
-    if (!request.headers["content-type"]?.startsWith("application/json")) {
-      return response.sendStatus(400);
-    }
-  }
-  next();
-};
